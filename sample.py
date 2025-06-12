9c5b248862ca7f08a08768a5abc26fb1e29d4aa5
Athena <martinathena28@gmail.com>
2025-06-12 09:52:46 +0530

    Initial commit

---
+++ sample.py
@@ -0,0 +1,17 @@
+# a=10
+# b=20
+# b=a+b
+# a=b-a
+# b=b-a
+# print("a=",a)
+# print("b=",b)
+
+# a=10
+# b=20
+# a,b=b,a
+# print("a={} b={}".format(a,b))
+# print(f"a={a} b={b}")
+
+a="hello"
+a=20
+print(type(a))
